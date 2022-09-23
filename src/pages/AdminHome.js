bab1049f9890ae206accb86d366d9a35f58fe1ca
emilhajizade42 <emil.ph@code.edu.az>
2022-09-23 09:08:29 +0400

    addded

--- src/pages/AdminHome.js
+++ src/pages/AdminHome.js
@@ -4,18 +4,32 @@ import { Link, Outlet } from 'react-router-dom'
 function AdminHome() {
   return (
     <div>
-        <nav>
-            <h1>Admin page</h1>
-            <hr/>
-            <Link to={"/home"}>home</Link>
-            <Link to={"/about"}>about</Link>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid">
+            <Link className='navbar-brand' to={"/home"}>Admin</Link>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <Link className='nav-link' to={"/home"}>Home</Link>
+                </li>
+                <li class="nav-item">
+                  <Link className='nav-link' to={"/about"}>About</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
         </nav>
-        <main>
-            <Outlet/>
-        </main>
-        <footer>
-            Salam
-        </footer>
+
+      <main className='h3 mt-3'>
+        <p className='py-3'>Adminnnn</p>
+        <Outlet />
+      </main>
+      <footer className='p-5 mt-5 bg-primary text-light'>
+        Admin Footer
+      </footer>
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,17 +4,31 @@ import { Link, Outlet } from 'react-router-dom'
 function Home() {
   return (
     <div>
-        <nav>
-            <h1>Home</h1>
-            <Link to={"/home"}>home</Link>
-            <Link to={"/about"}>about</Link>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid">
+            <Link className='navbar-brand' to={"/home"}>Home</Link>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <Link className='nav-link' to={"/home"}>Home</Link>
+                </li>
+                <li class="nav-item">
+                  <Link className='nav-link' to={"/about"}>About</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
         </nav>
-        <main>
-            <Outlet/>
-        </main>
-        <footer>
-            Salam
-        </footer>
+
+      <main className='h3 mt-3'>
+        <Outlet />
+      </main>
+      <footer className='p-5 mt-5 bg-primary text-light'>
+        Home Footer
+      </footer>
     </div>
   )
 }
